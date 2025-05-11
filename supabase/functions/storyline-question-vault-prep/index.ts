b426cfe0ca87408e37bef664f99af20075487888
Rong Feng Ye <rongfengyerongfengye@gmail.com>
2025-05-10 22:08:11 -0400

    update file names

--- supabase/functions/storyline-question-vault-prep/index.ts
+++ supabase/functions/storyline-question-vault-prep/index.ts
@@ -2,7 +2,7 @@
 import "https://deno.land/x/xhr@0.1.0/mod.ts";
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import { generateQuestion } from "./generate-question.ts";
-import { generateAnswer } from "./generate-answer.ts";
+import { generateAnswer } from "./generate-feedback.ts";
 
 export const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
